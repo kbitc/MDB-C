4e0805affa56481ee075b209928108d224f02a4c
mr_darker <mr_darker@darker.tech>
2019-02-06 16:31:44 -0600

    Updated to work with MDB.cpp global UART/Serial functions

--- Protocol_Files/Communication_Format.h
+++ Protocol_Files/Communication_Format.h
@@ -3,6 +3,7 @@
 
 #include "PreProcessors.h"
 #include "Bus_Timing.h"
+#include "MDB_ESP32.h"
 
 extern uint8_t chronoLogic(uint8_t command);
 
@@ -11,6 +12,7 @@ extern uint8_t checksum, pointer;
 extern uint8_t rX(uint8_t address);                        //Recieves a byte, judges if it needs to recieve the rest of the data, and ends at appropriate time.  Returns status codes.
 extern void clearBlock();                                  //Clear all communications related variables.
 extern uint8_t tX(unsigned int pointer, uint8_t address);  //Send the data in the block, calculate and insert check byte into location of pointer, uness pointer is zero.  ALways sets mode bit on last byte.
+extern uint8_t parityBitCalc(uint8_t raw);
 
 extern union nineBit {             //Union allows writing mode and data bits simultaneously as a short, therefore not overwriting one another.  Bit format: dddddddd_______m
   struct {                        //Structure allows accessing 8 data bits and 1 mode bit separately, as well as setting the parent union's size.
