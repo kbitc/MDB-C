f78479af31bcac8037d145f5388ee5419b5a0b1e
mr_darker <mr_darker@darker.tech>
2019-02-06 18:00:39 -0600

    Comments updated

--- Devices/Cashless Payment Device/Cashless.h
+++ Devices/Cashless Payment Device/Cashless.h
@@ -9,16 +9,16 @@ extern uint8_t chronoLogic(uint8_t command);
 
 extern unsigned int counter;
 extern uint8_t checksum, pointer;
-extern uint8_t rX(uint8_t address);                        //Recieves a byte, judges if it needs to recieve the rest of the data, and ends at appropriate time.  Returns status codes.
-extern void clearBlock();                                  //Clear all communications related variables.
-extern uint8_t tX(unsigned int pointer, uint8_t address);  //Send the data in the block, calculate and insert check byte into location of pointer, uness pointer is zero.  ALways sets mode bit on last byte.
+extern uint8_t rX(uint8_t address);                        /*Fetches incoming data, checks what to do with it, gets the full block if needed, reports what's up in its return value*/
+extern void clearBlock();                                  /*Clear all data out of the block, possibly unnecessary but will be kept for now*/
+extern uint8_t tX(unsigned int pointer, uint8_t address);  /*Send the data in the block, takes care of mode bits, and the check byte uness pointer is zero.  Calls rX() for responses*/
 extern uint8_t parityBitCalc(uint8_t raw);
 
-extern union nineBit {             //Union allows writing mode and data bits simultaneously as a short, therefore not overwriting one another.  Bit format: dddddddd_______m
-  struct {                        //Structure allows accessing 8 data bits and 1 mode bit separately, as well as setting the parent union's size.
-    uint8_t data : 8;             //8 data bits dddddddd most significant byte when written to as a short..
-    uint8_t mode : 1;             //1 mode bit  _______m least significant byte when written to as a short.
-    } part;                       //Named for readability & organization of code.
+extern union nineBit {             /*Union allows receiving 9 bit data as uint16_t*/
+  struct {                         /*Structure allows accessing 8 data bits and 1 mode bit separately*/
+    uint8_t data : 8;
+    uint8_t mode : 1;
+    } part;
     unsigned short whole;
 } block[35];                      /*Array of 9 bit data, MDB has a maximum block size of 36 "bytes"*/
 
