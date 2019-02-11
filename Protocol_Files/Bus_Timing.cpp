9f2659bd4eab912e27edc8cd54414730e3caf85b
mr_darker <mr_darker@darker.tech>
2019-02-11 15:58:36 -0600

    Indentation Fixed
    Code should follow Linux Kernel coding style.

--- Protocol_Files/Bus_Timing.cpp
+++ Protocol_Files/Bus_Timing.cpp
@@ -1,15 +1,17 @@
 #include "Bus_Timing.h"
 
-uint8_t chronoLogic(uint8_t command) {     /*Think of it like a stopwatch, but it returns a timing status rather than a time*/
-  if (command == TIMER_RESET) {
-      microseconds = micros();              //Save time of this command's execution.
-      return OKKK;
-  }
-  else if (command == TIMER_UPDATE) {
-    time_elapsed = micros() - microseconds;
-    if (time_elapsed > 200000) return T_SETUP;  /*Bus Reset is over*/
-    if (time_elapsed > 100000) return T_BREAK;  /*Bus Reset Timer*/
-    if (time_elapsed > 5000) return T_RESPONSE; /* #HardwareAdjust Adjusts these values according to your hardware's inaccuracies.  Verify with a Logic Analyzer*/
-    if (time_elapsed > 1000) return T_INTER_BYTE;
-  }
+uint8_t chronoLogic(uint8_t command)
+{
+        if (command == TIMER_RESET) {
+                microseconds = micros(); /*Each device has a local static copy of microseconds*/
+                return OKKK;
+        }
+        else if (command == TIMER_UPDATE) {
+                time_elapsed = micros() - microseconds;
+        /* #HardwareAdjust Verify timing with a logic analyzer and adjust values if necessary*/
+                if (time_elapsed > 200000) return T_SETUP;  /*Bus Reset is over*/
+                if (time_elapsed > 100000) return T_BREAK;  /*Bus Reset Timer  */
+                if (time_elapsed > 5000) return T_RESPONSE;
+                if (time_elapsed > 1000) return T_INTER_BYTE;
+        }                   
 }
