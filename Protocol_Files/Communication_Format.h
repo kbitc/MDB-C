8bb6621fd9faa37b1e0c1cd885f36e014bf2d80e
mr_darker <mr_darker@darker.tech>
2019-02-22 19:59:52 -0600

    Reformatting introduced
    Noted in source file committ

--- Protocol_Files/Communication_Format.h
+++ Protocol_Files/Communication_Format.h
@@ -15,11 +15,11 @@ extern uint8_t tX(unsigned int pointer, uint8_t address);  //Send the data in th
 extern uint8_t parityBitCalc(uint8_t raw);
 
 extern union nineBit {             //Union allows writing mode and data bits simultaneously as a short, therefore not overwriting one another.  Bit format: dddddddd_______m
-  struct {                        //Structure allows accessing 8 data bits and 1 mode bit separately, as well as setting the parent union's size.
-    uint8_t data : 8;             //8 data bits dddddddd most significant byte when written to as a short..
-    uint8_t mode : 1;             //1 mode bit  _______m least significant byte when written to as a short.
-    } part;                       //Named for readability & organization of code.
-    unsigned short whole;
-} block[35];                      /*Array of 9 bit data, MDB has a maximum block size of 36 "bytes"*/
+        struct {                        //Structure allows accessing 8 data bits and 1 mode bit separately, as well as setting the parent union's size.
+                uint8_t data : 8;             //8 data bits dddddddd most significant byte when written to as a short..
+                uint8_t mode : 1;             //1 mode bit  _______m least significant byte when written to as a short.
+        } part;                       //Named for readability & organization of code.
+        unsigned short whole;
+} mBlock[35], sBlock[35];                      /*Array of 9 bit data, MDB has a maximum block size of 36 "bytes"*/
 
 #endif
