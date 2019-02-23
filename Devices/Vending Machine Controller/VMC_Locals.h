63ac3ace94ebef4994deacd468e0d2476bb07797
mr_darker <mr_darker@darker.tech>
2019-02-22 19:57:57 -0600

    Nolonger used

--- Devices/Vending Machine Controller/VMC_Locals.h
+++
@@ -1,8 +0,0 @@
-struct peripheral {
-  uint8_t online : 1;
-  uint8_t featureLevel;
-  unsigned long pollTimer;
-  unsigned long  
-} peripheral;
-
-struct peripheral cashless1, cashless2, cashless3;
